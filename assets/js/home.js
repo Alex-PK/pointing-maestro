b967061dd0402ae5bd95be3be46fc7eb7a79daea
Alessandro Pellizzari <alessandro@workdigital.co.uk>
2017-07-19 09:39:05 +0100

    Improved homepage form style

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,26 +1,45 @@
 let app = new Vue({
     el: "#main",
     template: `
-        <div>
-            <h1>Home</h1>
+        <div class="container-fluid">
+            <div class="row">
+                <h1>Home</h1>
+            </div>
     
-            <form @submit.prevent="openRoom">
-                <label for="roomname">Create room:</label>
-                <input id="roomname" type="text" name="room" v-model="name" @keyUp="checkName" />
-                <span :value="error"></span>
-            </form>
+            <div class="row">
+                <div class="col">
+                    <form @submit.prevent="openRoom">
+                        <div class="form-group">
+                            <label for="roomname">Create room:</label>
+                            <input class="form-control" id="roomname" type="text" name="room" v-model="name" @keyup="checkName" />
+                            <small class="form-text text-danger">{{ error }}</small>
+                        </div>
+                        
+                        <button class="btn btn-primary" type="submit" :disabled="inError">Create</button>
+                    </form>
+                </div>
+            </div>
         </div>
     `,
+
     data: () => ({
         name: '',
         error: ''
     }),
+
+    computed: {
+        inError() {
+            return this.error.length > 0;
+        }
+    },
+
     methods: {
         checkName() {
-            if (!/[a-z0-9_-]/.test(this.name)) {
-                this.error = 'Invalid room name';
+            if (!/^[a-z0-9_-]+$/.test(this.name)) {
+                this.error = 'Please provide a valid room name';
                 return false;
             }
+            this.error = '';
             return true;
         },
 
