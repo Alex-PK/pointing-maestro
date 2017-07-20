fffaf9d953d8f854962b779240365262ec3b9a43
Alessandro Pellizzari <alessandro@workdigital.co.uk>
2017-07-20 09:11:17 +0100

    Implemented websockets

--- assets/js/room.js
+++ assets/js/room.js
@@ -1,6 +1,9 @@
-let app = new Vue({
-	el: "#main",
-	template: `
+(function (window, document, _, undefined) {
+	"use strict";
+
+	new Vue({
+		el: "#main",
+		template: `
 		<div class="container-fluid">
 			<div class="row">
 				<div class="col">
@@ -10,26 +13,73 @@ let app = new Vue({
 				</div>
 			</div>
 
-			<div class="row">
-				<div class="col">
+			<form>
+				<div class="form-group">
+					<label for="story-desc"></label>
+					<textarea id="story-desc" v-model="storyDesc"></textarea>
 				</div>
-			</div>
+			</form>
 		</div>
     `,
 
-	props: [
-		'name'
-	],
+		data: () => ({
+			name: null,
+			socket: null,
 
-	data: () => ({
-		socket: null
-	}),
+			storyDesc: '',
 
-	computed: {},
+			votes: {},
+		}),
 
-	mounted() {
+		watch: {
+			storyDesc: _.debounce(function() {
+				this.sendStoryDesc()
+			}, 500)
+		},
 
-	},
+		mounted() {
+			this.name = window.app.config.roomName;
+			this.socketConnect();
+		},
 
-	methods: {}
-});
+		methods: {
+			onSocketClose() {
+				console.log('socket closed', arguments);
+				this.socket = null;
+			},
+
+			onSocketMsg(msg) {
+				console.log('socket message', msg);
+			},
+
+			socketConnect() {
+				let wsUrl = window.location.hostname + ':' + window.location.port + '/msg/' + this.name;
+				let socket = new WebSocket('ws://' + wsUrl);
+
+				socket.onclose = this.onSocketClose;
+				socket.onmessage = this.onSocketMsg;
+
+				this.socket = socket;
+				console.log('socket opened', this.socket);
+			},
+
+			socketSend(msg) {
+				if (this.socket == null) {
+					console.log('socket was closed');
+					this.socketConnect();
+				}
+				this.socket.send(JSON.stringify(msg));
+			},
+
+			sendStoryDesc() {
+				let msg = {
+					cmd: 'storyDesc',
+					storyDesc: this.storyDesc
+				};
+				console.log('sending', msg);
+				this.socketSend(msg);
+			},
+
+		}
+	});
+})(window, document, _, undefined);
