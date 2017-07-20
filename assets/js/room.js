4b9b6d6fa53a87e9dd0bc1f9066a8f8d2a75efdd
Alessandro Pellizzari <alessandro@workdigital.co.uk>
2017-07-20 09:28:08 +0100

    Added vote buttons

--- assets/js/room.js
+++ assets/js/room.js
@@ -18,16 +18,29 @@
 					<label for="story-desc"></label>
 					<textarea id="story-desc" v-model="storyDesc"></textarea>
 				</div>
+				
+				<div class="form-group">
+					<div class="input-group">
+						<div v-for="v in availableVotes" class="input-group-btn">
+							<button class="btn btn-outline-primary" :class="{ active: v == vote }" type="button" @click.prevent="setVote(v)" style="margin-left: 0;">{{ v }}</button>
+      			</div>
+				</div>
 			</form>
 		</div>
     `,
+
+		props: {
+			availableVotes: {
+				default: [0, 1, 2, 3, 5, 8, 13, 21, '?']
+			}
+		},
 
 		data: () => ({
 			name: null,
 			socket: null,
 
 			storyDesc: '',
-
+			vote: null,
 			votes: {},
 		}),
 
@@ -68,6 +81,7 @@
 					console.log('socket was closed');
 					this.socketConnect();
 				}
+				console.log('sending', msg);
 				this.socket.send(JSON.stringify(msg));
 			},
 
@@ -76,9 +90,23 @@
 					cmd: 'storyDesc',
 					storyDesc: this.storyDesc
 				};
-				console.log('sending', msg);
+
 				this.socketSend(msg);
 			},
+
+			sendVote() {
+				let msg = {
+					cmd: 'vote',
+					vote: this.vote
+				};
+
+				this.socketSend(msg);
+			},
+
+			setVote(v) {
+				this.vote = v;
+				this.sendVote();
+			}
 
 		}
 	});
